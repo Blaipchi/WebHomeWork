a38c649da1317b5111baf2ae0981d9e587dce316
Starry516 <qiu.huiyang@foxmail.com>
2024-04-07 22:58:07 +0800

    Starry 2024/4/7 22:57

--- src/main/java/pojo/Article.java
+++ src/main/java/pojo/Article.java
@@ -1,63 +1,75 @@
 package pojo;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class Article {
-    String uid;
-    String artid;
-    String title;
-    String content;
-    String publicTime;
+    private Integer uid;
+    private Integer artid;
+    private String title;
+    private String content;
+    private Date publicTime;
 
     public Article()
     {
 
     }
 
-    public Article(String uid, String artid, String title, String content, String publicTime)
+    public Article(Integer uid, String title, String content)
     {
         this.uid = uid;
-        this.artid = artid;
         this.title = title;
         this.content = content;
-        this.publicTime = publicTime;
     }
 
-    public String getUid() {
-        return uid;
-    }
+//    public Article(String uid, String artid, String title, String content, String publicTime)
+//    {
+//        this.uid = uid;
+//        this.artid = artid;
+//        this.title = title;
+//        this.content = content;
+//        this.publicTime = publicTime;
+//    }
 
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public String getArtid() {
-        return artid;
-    }
-
-    public void setArtid(String artid) {
-        this.artid = artid;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getPublicTime() {
-        return publicTime;
-    }
-
-    public void setPublicTime(String publicTime) {
-        this.publicTime = publicTime;
-    }
+//    public String getUid() {
+//        return uid;
+//    }
+//
+//    public void setUid(String uid) {
+//        this.uid = uid;
+//    }
+//
+//    public String getArtid() {
+//        return artid;
+//    }
+//
+//    public void setArtid(String artid) {
+//        this.artid = artid;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    public String getPublicTime() {
+//        return publicTime;
+//    }
+//
+//    public void setPublicTime(String publicTime) {
+//        this.publicTime = publicTime;
+//    }
 }
--- src/main/java/pojo/User.java
+++ src/main/java/pojo/User.java
@@ -1,15 +1,18 @@
 package pojo;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class User {
-    String uid;
-    String username;
-    String password;
-    String email;
-    int age;
-    int flag;
-    List<pojo.Article> articleList;
+    private String uid;
+    private String username;
+    private String password;
+    private String email;
+    private Integer age;
+    private Integer flag;
+    List<Article> articleList;
 
     public User() {
 
@@ -24,63 +27,63 @@ public class User {
         this.articleList = articleList;
     }
 
-    public String getUid() {
-        return uid;
-    }
+//    public String getUid() {
+//        return uid;
+//    }
+//
+//    public void setUid(String uid) {
+//        this.uid = uid;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public int getFlag() {
+//        return flag;
+//    }
+//
+//    public void setFlag(int flag) {
+//        this.flag = flag;
+//    }
+//
+//    public List<pojo.Article> getArticleList() {
+//        return articleList;
+//    }
+//
+//    public void setArticleList(List<pojo.Article> articleList) {
+//        this.articleList = articleList;
+//    }
 
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getFlag() {
-        return flag;
-    }
-
-    public void setFlag(int flag) {
-        this.flag = flag;
-    }
-
-    public List<pojo.Article> getArticleList() {
-        return articleList;
-    }
-
-    public void setArticleList(List<pojo.Article> articleList) {
-        this.articleList = articleList;
-    }
-
-    public boolean checkRegister(String username, String password, String email, int age) {
+    public boolean checkRegister() {
         if (username.length() >= 6 && username.length() <= 20) {
             if (password.length() >= 6 && password.length() <= 20) {
                 if (email.length() >= 6 && email.length() <= 20) {
